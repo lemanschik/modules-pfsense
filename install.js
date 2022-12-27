6ea4c63aab1ec6b213f685147f3006e795a24803
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-27 11:28:48 +0100

    Create install.js

---
+++ install.js
@@ -0,0 +1,16 @@
+// A Minimal Installer
+
+// Assume NodeJS Environment for convinience.
+
+// link process io to chromium path to chromium import the installer script with a lemanschik/esm-loader like method from string. fetch.
+
+// chromium needs to be installed under /usr/local/bin/chromium-linux or chromium. 
+
+// connect to dspeed.eu/cloud/component-manager get routers back use router prefix learn ipv6 concepts.
+
+const pfSense = await import('dspeed.eu/cloud/component-manager/component-manager.js').then((router) => router.pfsense.install());
+pfSense.getReader().read(({ value, done }) => /* Your Code */);
+pfSense.getWriter().write(() => /* Your Code */); // Sends functions that return string instructions for pfsense internal.
+pfSense.getWriter().write(Promise.resolve(() => /* Your Code */)); // Sends functions that return string instructions for pfsense internal.
+pfSense.getWriter().write(() => ``)); // Sends functions that return string instructions for pfsense internal.
+// Strings can be prefixed with pfsense host else default Stealify Lang Module.
